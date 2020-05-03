b7ec16373f6dde343106d6852db4724fa463c99b
Konstantinos <konstantinos@feelrobotics.com>
2020-05-03 19:55:20 +0200

    WIP

--- src/green_magic/strain/__init__.py
+++ src/green_magic/strain/__init__.py
@@ -4,12 +4,3 @@ invoker = Invoker()
 
 from .data.receiver import Backend
 
-backends = ['df']
-b_objs = []
-
-for b in backends:
-    b_objs.append(Backend.build(b))
-
-
-data_master = object()
-data_master.
