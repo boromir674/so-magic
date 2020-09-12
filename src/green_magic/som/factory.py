3b058b8d994b5786a46bec5e207579b1a8d24768
Konstantinos <boromir674@hotmail.com>
2020-09-12 19:42:25 +0200

    refactor(som_factory): clean code

--- src/green_magic/som/factory.py
+++ src/green_magic/som/factory.py
@@ -4,29 +4,19 @@ from .self_organising_map import SomTrainer, SelfOrganizingMap
 
 
 @attr.s
-class SomFactory:
-    """Implementing from the BaseSomFactory allows other class to register/subscribe on (emulated) 'events'.
-       So, when the factory creates a new Som object, other entities can be notified."""
+class SelfOrganizingMapFactory:
     trainer = attr.ib(init=True, default=SomTrainer())
     subject = attr.ib(init=True, default=Subject([]))
 
-    def create_som(self, nb_cols, nb_rows, dataset, **kwargs):
+    def create(self, dataset, nb_cols, nb_rows, **kwargs):
         try:
-            map_obj = self.trainer.infer_map(nb_cols, nb_rows, dataset, **kwargs)
+            map_obj = self.trainer.infer_map(nb_cols, nb_rows, dataset, **kwargs)  # backend dependent (eg somoclu kind of object)
             self.subject.state = map_obj
             self.subject.notify()
-            return map_obj
+            return SelfOrganizingMap(map_obj, dataset.name)
         except NoFeatureVectorsError as e:
             logger.info(f"{e}. Fire up an 'encode' command.")
             raise e
-
-
-@attr.s
-class SelfOrganizingMapFactory:
-    som_factory = attr.ib(init=True, default=SomFactory())
-
-    def create(self, dataset, nb_cols, nb_rows, **kwargs):
-        return SelfOrganizingMap(self.som_factory.create_som(nb_cols, nb_rows, dataset, **kwargs), dataset.name)
 
 
 class NoFeatureVectorsError(Exception): pass
