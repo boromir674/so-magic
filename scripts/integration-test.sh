7174d5e4ca99a3bf85c2c2c73a2ab7d1a7270d58
Konstantinos <boromir674@hotmail.com>
2020-09-13 17:09:21 +0200

    try bash script for running integration test job

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -34,6 +34,21 @@ which conda
 
 echo '------------ CREATING ENV -------------'
 conda create -p $ENV_PATH -y
+
+printf "\n ---- SOURCING ----\n"
+source "$HOME/miniconda/etc/profile.d/conda.sh"
+
+printf "\n ---- HASHING ----\n"
+hash -r
+printf "\n ---- CONDA CONFIG ----\n"
+conda config --set always_yes yes --set changeps1 no
+printf "\n ---- CONDA UPDATE ----\n"
+conda update -q conda
+
+# Useful for debugging any issues with conda
+conda info -a
+
+
 echo '------------ ACTIVATING ENV -------------'
 conda activate $ENV_PATH
 
