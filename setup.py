f1343dbb407f48a4b74e304554256b1bdd70d19a
Konstantinos <boromir674@hotmail.com>
2020-10-02 19:06:45 +0300

    build(setuptools): clean setup.cfg and setup.py
    Previously, there were many commented out lines and also configurations in setup.cfg that got
    overriden by the ones in setup.py.

--- setup.py
+++ setup.py
@@ -9,29 +9,11 @@ from collections import OrderedDict
 my_dir = os.path.dirname(os.path.realpath(__file__))
 
 
-# CONSTANTS
+# Modify based on your project
 project_slug = 'so-magic'
-setup_cfg_filename = 'setup.cfg'
-readme_filename = 'README.rst'
 changelog_filename = 'CHANGELOG.rst'
 source_code_repo = f'https://github.com/boromir674/{project_slug}'
 changelog = f'{source_code_repo}/blob/dev/CHANGELOG.rst'
-
-README = os.path.join(my_dir, readme_filename)
-
-# Compute long description that will be rendered in the pypi server
-long_description = ''
-if not long_description:  # if not custom long description is supplied, then create one automatically
-    if not os.path.isfile(README):
-        long_description = 'Pending to provide with a long description for the software/package'
-    else:
-        with open(README) as f:
-            long_description = f.read()
-
-
-def requirements():
-    with open(os.path.join(my_dir, 'requirements', 'base.txt')) as fh:
-        return fh.read().split('\n')
 
 
 def run(cmd):
@@ -55,20 +37,18 @@ else:
     sys.exit(1)
 
 setup(
+    name=project_slug,
     version=_version,
-    description='library',
-    long_description=long_description,
     long_description_content_type='text/x-rst',
     test_suite='tests',
     install_requires=['attrs', 'numpy', 'scikit-learn', 'pandas', 'somoclu'],
     project_urls=OrderedDict([
-        ('1-Tracker', f'{source_code_repo}/issues'),
-        ('2-Changelog', changelog),
-        ('3-Source', source_code_repo),
-        ('4-Documentation', f'https://{project_slug}.readthedocs.io/en/dev/'), # "https://blahblah.readthedocs.io/en/v{}/".format(_version)
+        ('Issue Tracker', f'{source_code_repo}/issues'),
+        ('Changelog', changelog),
+        ('Source', source_code_repo),
+        ('Documentation', f'https://{project_slug}.readthedocs.io/en/dev/'), # "https://blahblah.readthedocs.io/en/v{}/".format(_version)
     ]),
-
-    # download_url='https://github.com/boromir674/music-album-creator/archive/v{}.tar.gz'.format(_version),  # help easy_install do its tricks
+    download_url=f'https://github.com/boromir674/so-magic/archive/v{_version}.tar.gz',  # help easy_install do its tricks
     # extras_require={},
     # setup_requires=[],
 )
