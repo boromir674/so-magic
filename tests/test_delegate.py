d028cc70349627c8360fc77cd5096e8650f533d5
boromir674 <boromir674@hotmail.com>
2021-06-17 04:49:13 +0300

    refactor(test_delegate): use a fixture to replace a for loop

--- tests/test_delegate.py
+++ tests/test_delegate.py
@@ -70,28 +70,28 @@ def built_in_n_client_backends(built_in_backends, client_pandas_tabular_implemen
     return built_in_backends
 
 
-def test_delegate_sanity_check(built_in_n_client_backends, tabular_operators, data_manager):
+@pytest.fixture(params=[
+    ['test-pd'],  # client backend
+    ['pd'],  # so magic built in (default) backend
+])
+def engine_backend(request, built_in_n_client_backends):
+    return built_in_n_client_backends.backends[request.param[0]]
+
+
+def test_delegate_sanity_check2(built_in_n_client_backends, engine_backend, tabular_operators, data_manager):
     dt_manager = data_manager()
-    # assert that the data engine initial (default) backend is "pandas-backend"
-    # could need to change in the future if we give the client the option to initialize the engine with a backend of
-    # their preference
     assert dt_manager.engine.backend.id == 'pd'
 
-    for backend_id, implementations_data in built_in_n_client_backends:
-        dt_manager.engine.backend = built_in_n_client_backends.backends[backend_id]
-        assert dt_manager.engine.backend.id == backend_id
-        assert all(all(required_method_name in dir(implementations_data[operator_interface_name])
-                       for required_method_name in required_methods)
-                   for operator_interface_name, required_methods in tabular_operators['required_methods'])
+    dt_manager.engine.backend = engine_backend
+    assert dt_manager.engine.backend.id == engine_backend.id
+    assert all(all(hasattr(getattr(engine_backend, operator_interface_name)(), required_method_name)
+                   for required_method_name in required_methods)
+               for operator_interface_name, required_methods in tabular_operators['required_methods'])
 
 
 def test_client_backend(built_in_n_client_backends, tabular_operators):
-
-    for operator_interface_name, required_methods in tabular_operators['required_methods']:
-        for m in required_methods:
-            nb_args = tabular_operators['get_nb_args'](operator_interface_name, m)
-            # we have to initialize an instance out of an operator class like we do in the 'observations_command' method in the Backend
-            # class (eg cls.retriever(), cls.iterator(), cls.mutator())
-            assert getattr(built_in_n_client_backends.implementations['test-pd'][operator_interface_name](), m)(*list([None] * nb_args)) == m
-
-        # assert all(getattr(built_in_n_client_backends.implementations['test-pd'][operator_interface_name], method)(None, None) == method for method in required_methods)
+    # just invoke all methods of each operator as a 'smoke test'
+    assert all(all(getattr(built_in_n_client_backends.implementations['test-pd'][operator_interface_name](), m)
+                   (*list([None] * tabular_operators['get_nb_args'](operator_interface_name, m))) == m
+                   for m in required_methods)
+               for operator_interface_name, required_methods in tabular_operators['required_methods'])
