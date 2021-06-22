d03ca4be391ddd29867eb0ea633eef58f41a3dd4
boromir674 <boromir674@hotmail.com>
2021-06-22 17:20:24 +0300

    refactor(test_command_registration): clean code and add a 'data building' fixture
    Previously, codacy reported Complexity = 20, sLoc / Method = 15.33 and Complexity / Method = 6.67.
    It also reported an added -3 overal Pull Request Complexity. This commit trties to reduce this
    numbers.

--- tests/test_command_registration.py
+++ tests/test_command_registration.py
@@ -7,47 +7,48 @@ def command_registrator():
     return CommandRegistrator
 
 
-def test_command_registrator(assert_different_objects, command_registrator):
+@pytest.fixture
+def classes(command_registrator):
     class A(metaclass=command_registrator): pass
     class B(metaclass=command_registrator): pass
     class C(B): pass
     class D(B): pass
-    classes = (A, B, C, D)
+    classes = type('Classes', (object,), {'A': A, 'B': B, 'C': C, 'D': D,
+                                          '__iter__': lambda self: iter([getattr(self, x) for x in 'ABCD'])})()
+    assert all([type(x) == command_registrator for x in classes])  # sanity check
+    assert all([isinstance(x, command_registrator) for x in classes])  # sanity check
+    return classes
 
+
+def test_command_registrator(assert_different_objects, classes):
     assert all([hasattr(x, 'registry') for x in classes])
-    assert all([type(x) == command_registrator for x in classes])
     assert all([hasattr(x, 'state') for x in classes])
-    assert_different_objects([A.registry, B.registry, C.registry, D.registry])
+    assert_different_objects([c.registry for c in classes])
 
-    A.state = 1
-    B.state = 2
-    C.state = 3
-    D.state = 4
-    assert_different_objects([A.state, B.state, C.state, D.state])
+    classes.A.state = 1
+    classes.B.state = 2
+    classes.C.state = 3
+    classes.D.state = 4
+    assert_different_objects([c.state for c in classes])
 
-    A.registry['a'] = 1
-    B.registry['b'] = 2
-    C.registry['c'] = 3
-    D.registry['d'] = 4
-    assert B.registry != A.registry != C.registry != D.registry
+    classes.A.registry['a'] = 1
+    classes.B.registry['b'] = 2
+    classes.C.registry['c'] = 3
+    classes.D.registry['d'] = 4
+    assert classes.B.registry != classes.A.registry != classes.C.registry != classes.D.registry
 
-    assert A.__getitem__('a') == 1
-    assert B.__getitem__('b') == 2
-    assert C.__getitem__('c') == 3
-    assert D.__getitem__('d') == 4
-    assert 'b' not in C.registry
-    assert 'c' not in B.registry
-    assert 'c' not in D.registry
-    assert 'd' not in C.registry
-    assert A['a'] == 1
-    assert B['b'] == 2
-    assert C['c'] == 3
-    assert D['d'] == 4
-
-    class P1(command_registrator): pass
-    assert type(P1) == type
-    assert not hasattr(P1, 'state')
-    assert not hasattr(P1, 'state')
+    assert classes.A.__getitem__('a') == 1
+    assert classes.B.__getitem__('b') == 2
+    assert classes.C.__getitem__('c') == 3
+    assert classes.D.__getitem__('d') == 4
+    assert 'b' not in classes.C.registry
+    assert 'c' not in classes.B.registry
+    assert 'c' not in classes.D.registry
+    assert 'd' not in classes.C.registry
+    assert classes.A['a'] == 1
+    assert classes.B['b'] == 2
+    assert classes.C['c'] == 3
+    assert classes.D['d'] == 4
 
 
 def test_wrong_command_registrator_usage(command_registrator):
