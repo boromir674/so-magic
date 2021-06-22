d32f292c29b997a745614e3877d56022b2b1798e
boromir674 <boromir674@hotmail.com>
2021-06-22 17:57:28 +0300

    refactor(test_cmd_registration): remove a data fixture and reduce code
    Previously, codacy reported Complexity = 20, sLoc / Method = 7.83 and Complexity / Method = 3.33. It
    also reported an added -3 overal Pull Request Complexity. This commit trties to reduce this numbers.

--- tests/test_command_registration.py
+++ tests/test_command_registration.py
@@ -2,12 +2,6 @@ import pytest
 
 
 @pytest.fixture
-def command_registrator():
-    from so_magic.data.backend.backend import CommandRegistrator
-    return CommandRegistrator
-
-
-@pytest.fixture
 def modify_registry():
     def _modify_registry(registry, key, value):
         registry[key] = value
@@ -15,19 +9,24 @@ def modify_registry():
 
 
 @pytest.fixture
-def classes(command_registrator):
-    class A(metaclass=command_registrator): pass
-    class B(metaclass=command_registrator): pass
+def classes():
+    from so_magic.data.backend.backend import CommandRegistrator
+
+    class A(metaclass=CommandRegistrator): pass
+    class B(metaclass=CommandRegistrator): pass
     class C(B): pass
     class D(B): pass
     classes = type('Classes', (object,), {'A': A, 'B': B, 'C': C, 'D': D,
                                           '__iter__': lambda self: iter([getattr(self, x) for x in 'ABCD'])})()
-    assert all([type(x) == command_registrator for x in classes])  # sanity check
-    assert all([isinstance(x, command_registrator) for x in classes])  # sanity check
-    return classes
+    assert all([type(x) == CommandRegistrator for x in classes])  # sanity check
+    assert all([isinstance(x, CommandRegistrator) for x in classes])  # sanity check
+
+    class P1(CommandRegistrator): pass
+    return {'correct': classes, 'wrong': P1}
 
 
 def test_command_registrator(assert_different_objects, classes, modify_registry):
+    classes = classes['correct']
     assert all([hasattr(x, 'registry') for x in classes])
     assert all([hasattr(x, 'state') for x in classes])
     assert_different_objects([c.registry for c in classes])
@@ -53,8 +52,7 @@ def test_command_registrator(assert_different_objects, classes, modify_registry)
     assert classes.D['d'] == 4
 
 
-def test_wrong_command_registrator_usage(command_registrator):
-    class P1(command_registrator): pass
-    assert type(P1) == type
-    assert not hasattr(P1, 'state')
-    assert not hasattr(P1, 'state')
+def test_wrong_command_registrator_usage(classes):
+    assert type(classes['wrong']) == type
+    assert not hasattr(classes['wrong'], 'state')
+    assert not hasattr(classes['wrong'], 'state')
