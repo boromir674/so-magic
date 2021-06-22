3dde831ab29740a7833717ca40fa50a710d9edaf
boromir674 <boromir674@hotmail.com>
2021-06-22 19:08:07 +0300

    refactor(test_decorating): split into 2 test cases and use a data building fixture
    Previously, codacy reported for test_decorating unit-test file: Complexity = 22, sLoC / Method =
    14.33 and Complexity / Method = 7.33. It also reported for the whole Pull request an added -17
    Complexity (pretty good!). This commit tries to reduce these numbers.

--- tests/test_decorating.py
+++ tests/test_decorating.py
@@ -1,44 +1,53 @@
+import pytest
 
-def test_decorating():
+
+@pytest.fixture
+def test_infra():
     from so_magic.data.backend.backend import MyDecorator, CommandRegistrator
+    return type('TestInfra', (object,), dict(MyDecorator=MyDecorator, CommandRegistrator=CommandRegistrator))
 
-    class A(MyDecorator): pass
-    class Ab(MyDecorator): pass
-    class B(metaclass=MyDecorator): pass
 
-    assert type(MyDecorator) == type
+def test_decorating(test_infra):
+    assert not hasattr(test_infra.CommandRegistrator, 'registry')
+    assert not hasattr(test_infra.CommandRegistrator, 'state')
+    assert not hasattr(test_infra.MyDecorator, '_commands_hash')
+
+    class A(test_infra.MyDecorator): pass
+    class Ab(test_infra.MyDecorator): pass
+    class B(metaclass=test_infra.MyDecorator): pass
+
+    assert type(test_infra.MyDecorator) == type
     assert type(A) == type
-    assert type(B) == MyDecorator
+    assert type(B) == test_infra.MyDecorator
 
-    RuntimeClass = MyDecorator('RuntimeClass', (object,), {})
-    assert type(RuntimeClass) == MyDecorator
+    RuntimeClass = test_infra.MyDecorator('RuntimeClass', (object,), {})
+    assert type(RuntimeClass) == test_infra.MyDecorator
     o1 = RuntimeClass()
     assert type(o1) == RuntimeClass
 
-    RuntimeClassA = MyDecorator('RuntimeClassA', (object,), {})
-    assert type(RuntimeClassA) == MyDecorator
+    RuntimeClassA = test_infra.MyDecorator('RuntimeClassA', (object,), {})
+    assert type(RuntimeClassA) == test_infra.MyDecorator
     o2 = RuntimeClassA()
     assert type(o2) == RuntimeClassA
 
     class ParentClass: pass
-    class DefinedClass(ParentClass, metaclass=MyDecorator): pass
+    class DefinedClass(ParentClass, metaclass=test_infra.MyDecorator): pass
     dc = DefinedClass()
     assert type(dc) == DefinedClass
 
     b = B()
     assert type(b) == B
 
-    objects = (MyDecorator, A, Ab, B, type(RuntimeClass), type(type(o2)), DefinedClass)
+    objects = (test_infra.MyDecorator, A, Ab, B, type(RuntimeClass), type(type(o2)), DefinedClass)
     assert all([hasattr(x, 'magic_decorator') for x in objects])
 
     assert id(A.magic_decorator) != id(Ab.magic_decorator)
     assert len(set([id(o.magic_decorator) for o in objects])) == 1
 
-    class ADecoratorClass(metaclass=CommandRegistrator): pass
-    class BDecoratorClass(metaclass=CommandRegistrator): pass
-    assert not hasattr(MyDecorator, '_commands_hash')
-    assert not hasattr(CommandRegistrator, 'registry')
-    assert not hasattr(CommandRegistrator, 'state')
+
+def test_decorator(test_infra):
+    class ADecoratorClass(metaclass=test_infra.CommandRegistrator): pass
+    class BDecoratorClass(metaclass=test_infra.CommandRegistrator): pass
 
     @ADecoratorClass.func_decorator()
     def aa(x):
