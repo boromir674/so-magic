9c7a6daf14738446907da67d8e6e96b8e96e8b72
Konstantinos <>
2021-06-24 17:25:54 +0300

    chore(semantic-version): bump package version from 0.6.1 to 0.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Konstantinos Lampridis'
 author = 'Konstantinos Lampridis'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.7.0'
 
 # -- General configuration ---------------------------------------------------
 
--- src/so_magic/__init__.py
+++ src/so_magic/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.6.1'
+__version__ = '0.7.0'
 
 from .so_master import SoMaster
 from .data import init_data_manager
